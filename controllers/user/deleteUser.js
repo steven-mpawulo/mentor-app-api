aa9a4cc661a267471e0dc9acbef1320f5c525910
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-30 14:48:06 +0300

    adds controller to delete user

---
+++ controllers/user/deleteUser.js
@@ -0,0 +1,23 @@
+const user = require("../../models/user")
+
+const deleteUser = async (req, res) => {
+    const userId = req.params.id;
+    console.log(userId);
+
+    if (userId !== null) { 
+        await user.findByIdAndDelete({ '_id': userId}).then((value) => {
+            console.log(value);
+            if(value !== null) {
+                res.status(200).json({"message": "user deleted", "user": value});
+            }
+        }).catch((e) => {
+            console.log(e);
+            res.status(400).json({"message": "failed to delete user"});
+        })
+    } else {
+        res.status(400).json({ "message": "please provide user id" });
+    }
+
+}
+
+module.exports = deleteUser;
