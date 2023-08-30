ab0adb2d139fc435a6315b569864e05ed46a598a
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-30 13:58:30 +0300

    adds and finishes up on get all users controller

--- controllers/user/getUsers.js
+++ controllers/user/getUsers.js
@@ -1,12 +1,14 @@
 const user = require("../../models/user")
 
-const getUsers = (req, res) => {
-    user.find({}).exec().then((value) => {
+const getUsers = async (req, res) => {
+    await user.find({}).exec().then((value) => {
         console.log(value);
         res.status(200).json({"message": "fetched all users", "users": value});
     }).catch((e) => {
         console.log(e);
         res.status(400).json({"message": "failed to fetch all users"});
-    })
+    });
 
 }
+
+module.exports = getUsers;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const user = require('../models/user');
 const createUser = require('../controllers/user/createUser');
+const getUsers = require('../controllers/user/getUsers');
 const userRoute = express.Router();
 
 
 userRoute.post('/users', createUser);
+userRoute.get('/users', getUsers);
 
 
 module.exports = userRoute;
