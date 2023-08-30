30ba79e93e0f404235928f20f69150fde6036a6c
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-30 14:21:35 +0300

    adds get user controller

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const user = require('../models/user');
 const createUser = require('../controllers/user/createUser');
 const getUsers = require('../controllers/user/getUsers');
+const getUser = require('../controllers/user/getUser');
 const userRoute = express.Router();
 
 
 userRoute.post('/users', createUser);
 userRoute.get('/users', getUsers);
+userRoute.get('/users/:id', getUser);
 
 
 module.exports = userRoute;
