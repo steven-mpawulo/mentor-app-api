1eb752e44c4559c4fe0bd221ea150ba37ff86bc9
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-30 12:32:54 +0300

    adds connection to database

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 5001;
-app.listen(port, () => {
-    console.log("server started");
+mongoose.connect(process.env.DB_URL).then((value) => {
+    console.log("database connected");
+    app.listen(port, () => {
+        console.log("server started");
+    });
+}).catch((e) => {
+console.log("database failed to connect");
 });
