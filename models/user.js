3600cbb1c1ca4df6397ac3e77707acee03549a9a
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-30 13:23:35 +0300

    modifies user schema

--- models/user.js
+++ models/user.js
@@ -20,7 +20,8 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         enum: ["MENTOR", "MENTEE", "ADMIN"],
-        default: "MENTEE"
+        default: "MENTEE",
+        required: true,
 
     },
 }, {timestamps: true});
