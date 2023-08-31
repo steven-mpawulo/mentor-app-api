a41fcbc98db6c6eb1b5b87f13efd4de019d14d7d
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-31 12:00:08 +0300

    adds update mentor controller

--- routes/mentorRoute.js
+++ routes/mentorRoute.js
@@ -3,12 +3,14 @@ const createMentor = require('../controllers/mentor/createMentor');
 const getMentors = require('../controllers/mentor/getMentors');
 const getMentor = require('../controllers/mentor/getMentor');
 const deleteMentor = require('../controllers/mentor/deleteMentor');
+const updateMentor = require('../controllers/mentor/updateMentor');
 const mentorRoute = express.Router();
 
 mentorRoute.post('/mentors/:id', createMentor);
 mentorRoute.get('/mentors', getMentors);
 mentorRoute.get('/mentors/:id', getMentor);
 mentorRoute.delete('/mentors/:id', deleteMentor);
+mentorRoute.put('/mentors/:id', updateMentor);
 
 
 module.exports = mentorRoute;
