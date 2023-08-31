dd78d2b58b13fcf6c545d0eb60ff7528a3ae1ccc
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-31 15:47:04 +0300

    adds delete mentee controller

--- routes/menteeRoute.js
+++ routes/menteeRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const createMentee = require('../controllers/mentee/createMentee');
 const getMentees = require('../controllers/mentee/getMentees');
 const getMentee = require('../controllers/mentee/getMentee');
+const deleteMentee = require('../controllers/mentee/deleteMentee');
 
 const menteeRoute = express.Router();
 
 menteeRoute.post('/mentees/:id', createMentee);
 menteeRoute.get('/mentees', getMentees);
 menteeRoute.get('/mentees/:id', getMentee);
+menteeRoute.delete('/mentees/:id', deleteMentee);
 
 module.exports = menteeRoute;
