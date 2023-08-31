e260b94df75d6304226b1f57266a03bd1267d690
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-31 11:09:39 +0300

    adds get mentor controller

--- routes/mentorRoute.js
+++ routes/mentorRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const createMentor = require('../controllers/mentor/createMentor');
 const getMentors = require('../controllers/mentor/getMentors');
+const getMentor = require('../controllers/mentor/getMentor');
 const mentorRoute = express.Router();
 
 mentorRoute.post('/mentors/:id', createMentor);
 mentorRoute.get('/mentors', getMentors);
+mentorRoute.get('/mentors/:id', getMentor);
 
 
 module.exports = mentorRoute;
