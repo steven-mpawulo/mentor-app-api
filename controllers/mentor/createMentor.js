08005da68a6fa48e3725edbd1c89006af7a94143
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-31 10:14:12 +0300

    finishes up on create mentor controller

---
+++ controllers/mentor/createMentor.js
@@ -0,0 +1,42 @@
+const mentor = require("../../models/mentor");
+const user = require("../../models/user");
+
+const createUser = async (req, res) => {
+    const userId = req.params.id;
+    const body = req.body;
+    const role = "MENTOR";
+
+    if (userId !== null) {
+        if (Object.keys(body).length !== 0) {
+            await user.findByIdAndUpdate({ '_id': userId }, { $set: { "role": role } }, { new: true }).then(async (value) => {
+                console.log(value);
+                const mentor = new mentor({
+                    "firstName": body.firstName,
+                    "lastName": body.lastName,
+                    "email": body.email,
+                    "userId": value._id,
+                    "expertise": body.expertise,
+                });
+                await mentor.save().then((value) => {
+                    console.log(value);
+                    res.status(201).json({ "message": "mentor created successfully", "mentor": value });
+                }).catch((e) => {
+                    console.log(e);
+                    res.status(400).json({ "message": "failed to create mentor" });
+                });
+
+            }).catch((e) => {
+                console.log(e);
+                res.status(400).json({ "message": "user not found" });
+            });
+
+        } else {
+            res.status(400).json({ "message": "please provide mentor data" });
+        }
+
+    } else {
+        res.status(400).json({ "message": "please provide user id" });
+    }
+}
+
+module.exports = createUser;
