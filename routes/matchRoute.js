959a948f11ab113c4711543a04e6b729d4e1660c
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-01 12:34:29 +0300

    adds delete match controller

--- routes/matchRoute.js
+++ routes/matchRoute.js
@@ -2,12 +2,14 @@ const express = require('express');
 const getMatched = require('../controllers/match/getMatched');
 const getMatches = require('../controllers/match/getMatches');
 const getMatch = require('../controllers/match/getMatch');
+const deleteMatch = require('../controllers/match/deleteMatch');
 
 const matchRoute = express.Router();
 
 matchRoute.post('/match/:id/:id2', getMatched);
 matchRoute.get('/match', getMatches);
 matchRoute.get('/match/:id', getMatch);
+matchRoute.delete('/match/:id', deleteMatch);
 
 
 module.exports = matchRoute;
