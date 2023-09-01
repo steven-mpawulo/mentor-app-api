7b316dee65b23233123821c6593cc61c3388f35f
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-01 15:31:43 +0300

    works on uodating chat

--- controllers/chat/updateChat.js
+++ controllers/chat/updateChat.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const chat = require('../../models/chat');
+const message = require('../../models/message');
 const updateChat = async (req, res) => {
     const mentorId = mongoose.Types.ObjectId(req.params.id);
     const menteeId = mongoose.Types.ObjectId(req.params.id2);
@@ -7,19 +8,40 @@ const updateChat = async (req, res) => {
     console.log(body);
 
     if (mentorId !== null && menteeId !== null) {
-
+          let sender;
+          let receiver;
         if (Object.keys(body).length !== 0) {
-            await chat.findOneAndUpdate({'between': [mentorId, menteeId]}, {$addToSet: {
-                "messages": message,
-            }}, {new: true}).then((value) => {
-                console.log(value);
-                res.status(201).json({"message": "message added"});
-                
-            }).catch((e) => {
-                console.log(e);
-                res.status(400).json({"message": "failed to add message"});
+            if (body.sender === "mentor") {
+                sender = mentorId;
+                receiver = menteeId;
+            } else if (body.sender === "mentee"){
+                sender = menteeId;
+                receiver = mentorId;
+            } else {
+                res.status(400).json({"message": "please provide sender value as mentor or mentee"});
+            }
+            const newMessage = message({
+                message: body.message,
+                sender: sender,
+                receiver:receiver,
             });
 
+            await newMessage.save().then( async (value) => {
+                console.log(value);
+                await chat.findOneAndUpdate({'between': [mentorId, menteeId]}, {$addToSet: {
+                    "messages": value,
+                }}, {new: true}).then((value) => {
+                    console.log(value);
+                    res.status(201).json({"message": "message added", "chat": value});
+                    
+                }).catch((e) => {
+                    console.log(e);
+                    res.status(400).json({"message": "failed to add message"});
+                });
+            }).catch((e) => {
+                console.log(e);
+                res.status(400).json({"message": "failed to save message"});
+            });
         } else {
             res.status(400).json({"message": "please provide a message"});
         }
