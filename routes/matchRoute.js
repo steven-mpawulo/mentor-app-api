41773a6d3c56763a2ffba6804134ee6eed915809
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-01 12:16:55 +0300

    adds get match controller

--- routes/matchRoute.js
+++ routes/matchRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const getMatched = require('../controllers/match/getMatched');
 const getMatches = require('../controllers/match/getMatches');
+const getMatch = require('../controllers/match/getMatch');
 
 const matchRoute = express.Router();
 
 matchRoute.post('/match/:id/:id2', getMatched);
 matchRoute.get('/match', getMatches);
+matchRoute.get('/match/:id', getMatch);
 
 
 module.exports = matchRoute;
