7814912695985799fd0ed10e921f999d67464d97
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-01 13:09:53 +0300

    works on create chat controller

---
+++ controllers/chat/createChat.js
@@ -0,0 +1,51 @@
+const chat = require("../../models/chat");
+const match = require("../../models/match");
+
+const createChat = async (req, res) => {
+    const mentorId = req.params.id;
+    const menteeId = req.params.id2;
+
+    if (mentorId !== null && menteeId !== null) {
+        await chat.findOne({ 'between': [mentorId, menteeId] }).exec().then(async (value) => {
+            console.log(value);
+            if (value === null) {
+
+                await match.findOne({ 'between': [mentorId, menteeId] }).then(async (value) => {
+                    if(value !== null) {
+                        console.log(value);
+
+                        const newChat = chat({
+                            between: [mentorId, menteeId],
+                        });
+
+                        await newChat.save().then((value) => {
+                            console.log(value);
+                            res.status(201).json({"message": "chat created", "chat": value});
+                        }).catch((e) => {
+                            console.log(e);
+                            res.status(400).json({"message": "failed to create chat"});
+                        });
+
+                    } else {
+                        res.status(400).json({'message': "please first match with mentor or mentee"});
+                    }
+                 }).catch((e) => { 
+                    console.log(e);
+                    res.status(400).json({"message": "failed to find match"});
+                 });
+
+            } else {
+                res.status(400).json({ "message": "chat already exists" });
+            }
+        }).catch((e) => { 
+            console.log(e);
+            res.status(400).json({"message": "failed to create chat"});
+        });
+
+    } else {
+        res.status(400).json({ "message": "please provide mentor and mentee id" })
+    }
+
+}
+
+module.exports = createChat;
