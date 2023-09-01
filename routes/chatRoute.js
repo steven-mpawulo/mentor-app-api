498ab53aa4ebe8ed829399d6a44969da67ca314f
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-01 15:32:53 +0300

    adds update chat controller

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const createChat = require('../controllers/chat/createChat');
+const updateChat = require('../controllers/chat/updateChat');
 
 const chatRoute = express.Router();
 
 chatRoute.post('/chats/:id/:id2', createChat);
+chatRoute.put('/chats/:id/:id2', updateChat);
 
 module.exports = chatRoute;
