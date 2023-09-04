0b4059e411763d68e2e224b044a2de80d0dbde1f
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-04 10:08:36 +0300

    logs all chat values fo error checking

--- controllers/chat/getChats.js
+++ controllers/chat/getChats.js
@@ -2,6 +2,7 @@ const chat = require("../../models/chat")
 
 const getChats = async (req, res) => {
     await chat.find({}).exec().then((value) => {
+        console.log(value);
         if (value !== null) {
             res.status(200).json({"message": "successfully fetched all chats", "chats": value});
         } else {
