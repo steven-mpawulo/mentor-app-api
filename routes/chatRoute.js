4d601401dc5d44bb268bbcf0139a4ae1dc9e6bad
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-04 09:41:19 +0300

    connects delete chat controller to chat route

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const createChat = require('../controllers/chat/createChat');
 const updateChat = require('../controllers/chat/updateChat');
+const deleteChat = require('../controllers/chat/deleteChat');
 
 const chatRoute = express.Router();
 
 chatRoute.post('/chats/:id/:id2', createChat);
 chatRoute.put('/chats/:id/:id2', updateChat);
+chatRoute.delete('/chats/:id', deleteChat);
 
 module.exports = chatRoute;
