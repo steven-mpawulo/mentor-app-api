b394fb978df0b55dc099a2b422c3ca231a0afa77
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-04 09:56:18 +0300

    adds get chat controller to chat route

--- controllers/chat/getChat.js
+++ controllers/chat/getChat.js
@@ -18,3 +18,5 @@ const getChat = async (req, res) => {
         });
     }
 }
+
+module.exports = getChat;
--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const createChat = require('../controllers/chat/createChat');
 const updateChat = require('../controllers/chat/updateChat');
 const deleteChat = require('../controllers/chat/deleteChat');
+const getChat = require('../controllers/chat/getChat');
 
 const chatRoute = express.Router();
 
 chatRoute.post('/chats/:id/:id2', createChat);
 chatRoute.put('/chats/:id/:id2', updateChat);
 chatRoute.delete('/chats/:id', deleteChat);
+chatRoute.get('/chats/:id', getChat);
 
 module.exports = chatRoute;
