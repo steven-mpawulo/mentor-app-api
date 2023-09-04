216365237c442a5ae3ce1fcde8ba852e95e79edb
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-04 11:25:50 +0300

    adds auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const signup = require('../controllers/auth/signup');
+const signin = require('../controllers/auth/signin');
 
 const authRoute = express.Router();
 
 authRoute.post('/signup', signup);
+authRoute.post('/signin', signin);
 
 module.exports = authRoute;
