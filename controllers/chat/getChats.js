766df666e63b9720937ec96cbd68f866f410ef3f
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-04 10:05:45 +0300

    creates get chats controller

---
+++ controllers/chat/getChats.js
@@ -0,0 +1,17 @@
+const chat = require("../../models/chat")
+
+const getChats = async (req, res) => {
+    await chat.find({}).exec().then((value) => {
+        if (value !== null) {
+            res.status(200).json({"message": "successfully fetched all chats", "chats": value});
+        } else {
+            res.status(400).json({"message": "no chats found"});
+        }
+
+    }).catch((e) => {
+        console.log(e);
+        res.status(400).json({"message": "something went wrong"});
+    });
+}
+
+module.exports = getChats;
