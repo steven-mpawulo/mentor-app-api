d2c253638766bb58875f45191376f57534d5d0c6
steven-mpawulo <stevenmpawulo@gmail.com>
2023-09-08 14:45:01 +0300

    adds get message controller to chat route

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -6,6 +6,7 @@ const getChat = require('../controllers/chat/getChat');
 const getChats = require('../controllers/chat/getChats');
 const deleteMessage = require('../controllers/message/deleteMessage');
 const updateMessage = require('../controllers/message/updateMessage');
+const getMessage = require('../controllers/message/getMessage');
 
 const chatRoute = express.Router();
 
@@ -16,6 +17,7 @@ chatRoute.get('/chats/:id', getChat);
 chatRoute.get('/chats', getChats);
 chatRoute.delete('/chats/:id/messages/:id2', deleteMessage);
 chatRoute.put('/chats/:id/messages/:id2', updateMessage);
+chatRoute.get('/chats/:id/messages/:id2', getMessage);
 
 
 module.exports = chatRoute;
